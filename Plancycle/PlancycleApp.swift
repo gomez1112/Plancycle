2c6076e318b0eaeed1cf60561330b9176db9b8de
gomez1112 <54489633+gomez1112@users.noreply.github.com>
2026-06-07 19:27:04 -0400

    Initial Commit

---
+++ Plancycle/PlancycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlancycleApp.swift
+//  Plancycle
+//
+//  Created by Gerard Gomez on 6/7/26.
+//
+
+import SwiftUI
+
+@main
+struct PlancycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
